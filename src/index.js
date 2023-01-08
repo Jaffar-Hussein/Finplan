01cbb7d47e8ff95a98eeff60c0a585f4aca1e4a3
Sam Arita <arita.sam@student.moringaschool.com>
2023-01-08 22:49:57 +0300

    feat: added routing feature

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router} from "react-router-dom"
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Router>
+  <App />
+</Router>);
+
 
 reportWebVitals(sendToVercelAnalytics);
