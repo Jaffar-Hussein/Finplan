a2be50685aa6e4f80f6fdf55ebd7cb37df0c30fd
Jaffar Hussein <jaffhussein@icloud.com>
2023-01-12 00:22:26 +0300

    login styling

--- src/components/login.js
+++ src/components/login.js
@@ -1,55 +1,55 @@
-import React from "react";
 import '../style.css';
-function Login(){
-    return (
-        <div class="row g-0">
-        <div class="col-md-8 col-lg-6 signup">
-          <div class="login d-flex align-items-center py-5 plusafya">
-            <div class="container">
-              <div class="row ">
-                <div class="col-md-9 col-lg-8   mx-auto ">
-                  <p class="h2 my-5 afya">Welcome to AfyaPlus</p>
-                  <h3 class="login-heading mb-4" >Sign In</h3>
-                  <form (ngSubmit)="onLoggedin()" class="box">
-                    <div class="form-floating mb-3">
-                      <input type="email" [(ngModel)]="login.username" name="username" class="form-control" id="floatingInput" placeholder="name@example.com">
-                      <label for="floatingInput">username</label>
+function Login() {
+  return (
+    <>
+      <div className="row g-0">
+        <div className="d-none d-md-flex col-md-4 col-lg-6 bg-image ">
+        </div>
+        <div className="col-md-8 col-lg-6 signup">
+          <div className="login d-flex align-items-center py-5 plusafya">
+            <div className="container">
+              <div className="row ">
+                <div className="col-md-9 col-lg-8 mx-auto ">
+                  <h3 className="login-heading mb-4" >Sign In</h3>
+                  <form className="box" role="form">
+                    <div className="form-floating mb-3">
+                      <input type="email" name="username" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                      <label>username</label>
                     </div>
-                    <div class="form-floating mb-3">
-                      <input type="password" [(ngModel)]="login.password" name="password" class="form-control" id="floatingPassword" placeholder="Password">
-                      <label for="floatingPassword">Password</label>
+                    <div className="form-floating mb-3">
+                      <input type="password" name="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                      <label>Password</label>
                     </div>
-    
-                    <div class="form-check mb-3">
-                      <input class="form-check-input"  style="background-color: #005F73;" type="checkbox" value="" id="rememberPasswordCheck">
-                      <label class="form-check-label"  for="rememberPasswordCheck">
+
+                    <div className="form-check mb-3">
+                      {/* <input className="form-check-input" style={{backgroundColor: '#005F73'}} type="checkbox" value="" id="rememberPasswordCheck" /> */}
+                      <label className="form-check-label" >
                         I agree to terms and condition
                       </label>
                     </div>
-    
-                    <div class="d-grid form mb-3">
-                      <button class="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="submit">Sign in</button>
-                      <div class="text-center">
+
+                    <div className="d-grid form mb-3">
+                      <button className="btn btn-lg btn-primary btn-login text-uppercase fw-bold mb-2" type="submit">Sign in</button>
+                      <div className="text-center">
                       </div>
                     </div>
-                    <div class="mb-12 mt-3 ml-5">
-                        <small class="text-muted float-left">
-                            Don't have an Account? <a class="ml-2" routerLink="/register" type="button" style="text-decoration: none;color: #005F73;">Create Account</a>
-                        </small>
+                    <div className="mb-12 mt-3 ml-5">
+                      {/* <small className="text-muted float-left">
+Don't have an Account? <a className="ml-2" type="button" style={{textDecoration: 'none',color: '#005F73'}}>Create Account</a>
+</small> */}
                     </div>
-             
+
                   </form>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image ">
-        </div>
-       
       </div>
-    );
+    </>
+
+  );
 
 }
 
-export default Login
+export default Login;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import {BrowserRouter as Router} from "react-router-dom"
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
