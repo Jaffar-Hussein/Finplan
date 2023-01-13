790490b846b3806d953322c37ae09c65ec5003d5
nxoo <nxoo.dev@gmail.com>
2023-01-14 02:02:06 +0300

    removed navbar from login&signup page

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
     return (
         <Router>
             <Routes>
+                <Route path='/signup' element={<Signup/>}/>
+                <Route path='/login' element={<Login/>}/>
                 <Route path='/' element={<Navbar/>}>
-                    <Route path='/signup' element={<Signup/>}/>
-                    <Route path='/login' element={<Login/>}/>
                     <Route path='*' element={<NotFound/>}/>
                 </Route>
             </Routes>
