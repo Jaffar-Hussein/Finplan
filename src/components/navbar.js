9a47f3fe330781e54b5975ecfd2b8e2ae825c65c
nxoo <nxoo.dev@gmail.com>
2023-01-14 02:12:29 +0300

    reset boolean

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -38,7 +38,7 @@ export default function Navbar() {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0 fw-semibold">
-                            {!user ?
+                            {user ?
                                 <>
                                     <li className="nav-item">
                                         <a
